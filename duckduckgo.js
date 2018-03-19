4d7c640055208dd138bc784f6b0e22651b070c12
Zack Scholl <zack.scholl@gmail.com>
2018-03-18 23:54:52 -0700

    First commit

---
+++ duckduckgo.js
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const puppeteer = require('puppeteer');
+
+(async() => {
+
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+await page.goto('https://duckduckgo.com/?q=brownie+recipe&t=hy&ia=recipes', {waitUntil: 'networkidle2'});
+await page.setViewport({
+    width: 1200,
+    height: 800
+});
+
+
+// Wait for the results to show up
+await page.waitForSelector('h2 a');
+
+await autoScroll(page);
+await page.screenshot({
+    path: 'jd.png',
+    fullPage: true
+});
+
+// Extract the results from the page
+const links = await page.evaluate(() => {
+  const anchors = Array.from(document.querySelectorAll('h2 a.result__a'));
+  return anchors.map(anchor => anchor.href);
+});
+console.log(links.join('\n'));
+await browser.close();
+
+})();
+
+
+function autoScroll(page){
+    return page.evaluate(() => {
+        return new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if(totalHeight >= scrollHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        })
+    });
+}
