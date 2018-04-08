0fb90ba3a963918cbde50681c25623b15cc66f7d
Zack Scholl <zack.scholl@gmail.com>
2018-04-07 23:53:43 -0700

    Add survey

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/AlecAivazis/survey"
 	meanrecipe "github.com/schollz/meanrecipe/src"
 )
 
@@ -18,6 +19,29 @@ func main() {
 	flag.BoolVar(&debug, "debug", false, "set debug")
 	flag.IntVar(&clusters, "clusters", 30, "number of clusters to generate")
 	flag.Parse()
+
+	if len(recipe) == 0 {
+		// the questions to ask
+		var qs = []*survey.Question{
+			{
+				Name:      "recipe",
+				Prompt:    &survey.Input{Message: "What recipe (e.g. chocolate chip cookies)?"},
+				Validate:  survey.Required,
+				Transform: survey.Title,
+			},
+		} // the answers will be written to this struct
+		answers := struct {
+			Recipe string // survey will match the question and field names
+		}{}
+
+		// perform the questions
+		err = survey.Ask(qs, &answers)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		recipe = strings.ToLower(strings.TrimSpace(answers.Recipe))
+	}
 
 	if debug {
 		meanrecipe.SetLogLevel("debug")
